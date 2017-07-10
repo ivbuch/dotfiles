df4eeb349c0cbf4da93aa612e6beb428bb6d59ef
Igor Buchatsky <ivbuchatsky@gmail.com>
2017-07-10 15:58:41 +0300

    update

---
+++ tmux/tmux-perfomance-monitor.py
@@ -0,0 +1,34 @@
+#!/usr/local/bin/python3
+import os
+import subprocess
+
+SESSION_NAME="perfomance"
+
+def call_tmux_command(args):
+    process = subprocess.run(["tmux"] + args, stdout=subprocess.PIPE)
+
+def tmux_attach():    
+    call_tmux_command(["select-window", "-t", SESSION_NAME + ":top"])
+    call_tmux_command(["attach", "-t", SESSION_NAME])
+
+def tmux_open_windows():    
+    call_tmux_command(["send-keys", "-t", SESSION_NAME + ":top", "top", "Enter"])
+
+def tmux_open_session():    
+    call_tmux_command(["new", "-s", SESSION_NAME, "-d", "-n","top"])
+
+def main():
+    tmux_open_session()
+    tmux_open_windows()
+    rename_i3_workspace()
+    tmux_attach()
+
+def rename_i3_workspace():
+    import i3ipc
+    i3 = i3ipc.Connection()
+    workspaces = i3.get_workspaces()
+    current_workspace = next(filter(lambda w: w.focused, workspaces))
+    i3.command('rename workspace "{}" to {}'.format(current_workspace.name, "perfomance"))
+
+if __name__ == "__main__":
+    main()
