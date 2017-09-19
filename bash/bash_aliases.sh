5239b58e18836dd646af32c5125bd8b3704a4c6a
Igor Buchatsky <ivbuchatsky@gmail.com>
2017-09-19 23:52:20 +0300

    update

--- bash/bash_aliases.sh
+++ bash/bash_aliases.sh
@@ -1,6 +1,7 @@
 alias weather="curl wttr.in"
 alias tma="/my-tools/home-infostructure/python-scripts/tmux/tmux-attach-session.py"
 alias tx="tmuxp load"
+alias txl="ls ~/.tmuxp/"
 alias ranger='ranger --choosedir=$HOME/rangerdir; LASTDIR=`cat $HOME/rangerdir`; cd "$LASTDIR"'
 alias r='ranger --choosedir=$HOME/rangerdir; LASTDIR=`cat $HOME/rangerdir`; cd "$LASTDIR"'
 alias ja='. jira-accident-folder.sh'
