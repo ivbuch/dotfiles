832ef3005baa09941d2d0476fb7d2e6d1aad96f8
Igor Buchatsky <ivbuchatsky@gmail.com>
2017-09-30 14:32:30 +0300

    update

--- bash/mount-seagate-externaldrive.sh
+++ bash/mount-seagate-externaldrive.sh
@@ -1,4 +1,4 @@
 DEST="/mnt/seagate-drive"
 sudo mkdir -p -v $DEST
-sudo mount -t vfat /dev/sdd1 $DEST -o uid=1000,gid=1000,utf8,dmask=027,fmask=137
+sudo mount -t ntfs /dev/sdd1 $DEST
 echo $DEST mounted
