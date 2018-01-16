27b6ee337b5ba4be91aef75ff2bc60615febae23
Igor Buchatskiy <igor.buchatskiy@medfusion.com>
2018-01-16 08:54:32 -0500

    update

--- bash/bash_aliases.sh
+++ bash/bash_aliases.sh
@@ -44,7 +44,7 @@ alias cdh="cd /my-tools/home-infostructure"
 alias cda="cd /my-tools/ansible"
 alias cdap="cd /my-tools/ansible/playbooks"
 alias cdw="cd ~/ideaProjects"
-alias cdai="cd $HOME/workspace/apps/identity-server"
+alias cdai="cd $HOME/workspace/mfss-embeddable/apps/identity-server"
 
 # init dotfiles
 alias init_dotfiles="cd /my-tools/dotfiles/ansible && ansible-playbook init-dotfiles.yml --ask-sudo-pass"
