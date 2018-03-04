1382a852bd655da7061010fc308380490e8c3e29
ivbuch <ivbuch@ivbuch>
2018-03-04 15:12:19 -0500

    update

--- bash/thinkpad/change_backlight.sh
+++ bash/thinkpad/change_backlight.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 FN="/sys/class/backlight/intel_backlight/brightness"
-VALUE=$(cat $FN)
-STEP=50
+VALUE="$(cat $FN)"
+MAX="$(cat /sys/class/backlight/intel_backlight/max_brightness)"
+STEP="50"
+MIN="50"
 if [[ $1 == "increment" ]]; then
-  UPDATED=$(($VALUE + $STEP))
+  UPDATED=$((VALUE + STEP))
+  if [ $UPDATED -gt "$MAX" ]; then
+    UPDATED="$MAX"
+  fi
 fi
 
 if [[ $1 == "decrement" ]]; then
   UPDATED=$(($VALUE - $STEP))
+  if [ $UPDATED -lt "$MIN" ]; then
+    UPDATED="$MIN"
+  fi
 fi
 
 echo $UPDATED > $FN
