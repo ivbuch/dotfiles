43cf109a21106632c0b02739281bbafaf99d6987
Igor Buchatskiy <igor.buchatskiy@medfusion.com>
2018-05-01 17:20:46 -0400

    update

--- bash/bash_aliases.sh
+++ bash/bash_aliases.sh
@@ -100,3 +100,6 @@ alias sdu="systemctl --user"
 
 # WORK
 alias rc='$HOME_INFOSTRUCTURE/work/medfusion/rest/rest-client/main.py' 
+
+# KARMA
+alias js_karma="node_modules/karma/bin/karma start karma.conf.js"
