f32122f5f6071f5acc2408c892b6668ff5a869da
ivbuch <ivbuch@ivbuch>
2018-11-07 22:13:52 -0500

    update

--- bash/zshrc.sh
+++ bash/zshrc.sh
@@ -3,6 +3,7 @@ export SPRING_HOME="/opt/spring-boot/actual"
 export DISABLE_AUTO_TITLE="true"
 export DOT_FILES="/my-tools/dotfiles"
 export HOME_INFOSTRUCTURE="/my-tools/home-infostructure"
+export SPARK_HOME="/opt/apache-spark/actual"
 
 # for karma test runner
 export "CHROME_BIN=$(which chromium)"
@@ -10,17 +11,18 @@ export "CHROME_BIN=$(which chromium)"
 # makes less search case insensitive
 export LESS="-i -R"
 
-export PATH=$PATH:$HOME_INFOSTRUCTURE/python-scripts/docker
-export PATH=$PATH:$HOME_INFOSTRUCTURE/python-scripts/svn
-export PATH=$PATH:$HOME_INFOSTRUCTURE/python-scripts/tmux
-export PATH=$PATH:$HOME_INFOSTRUCTURE/python-scripts/git
-export PATH=$PATH:$HOME_INFOSTRUCTURE/python-scripts/i3
-export PATH=$PATH:$HOME_INFOSTRUCTURE/bash-scripts/work
-export PATH=$PATH:/opt/translate-script
-export PATH=$PATH:$SPRING_HOME/bin
+export PATH="$PATH:$HOME_INFOSTRUCTURE/python-scripts/docker"
+export PATH="$PATH:$HOME_INFOSTRUCTURE/python-scripts/svn"
+export PATH="$PATH:$HOME_INFOSTRUCTURE/python-scripts/tmux"
+export PATH="$PATH:$HOME_INFOSTRUCTURE/python-scripts/git"
+export PATH="$PATH:$HOME_INFOSTRUCTURE/python-scripts/i3"
+export PATH="$PATH:$HOME_INFOSTRUCTURE/bash-scripts/work"
+export PATH="$PATH:/opt/translate-script"
+export PATH="$PATH:$SPRING_HOME/bin"
+export PATH="$PATH:$SPARK_HOME/bin"
 
-source ~/.bash_aliases
-source $DOT_FILES/bash/bash_functions.sh
+source "$HOME/.bash_aliases"
+source "$DOT_FILES/bash/bash_functions.sh"
 
 
 VIRTUAL_ENV_DISABLE_PROMPT=1 source $HOME/.config/virtual-envs/main/bin/activate
