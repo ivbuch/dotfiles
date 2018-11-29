b85ade8a0f45e13c6cb8933c8eac5d4b3d89c9d4
Igor Buchatskiy <igor.buchatskiy@medfusion.com>
2018-11-29 13:38:26 -0500

    update

--- i3blocks/module-git.sh
+++ i3blocks/module-git.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # cd "$DOT_FILES"
-cd "$HOME_INFOSTRUCTURE"
-git status -v 2>&1 | grep "nothing to commit" > /dev/null
+cd "/my-tools/home-infostructure"
+git status 2>&1 | grep "nothing to commit" > /dev/null
 hi_s="$?"
 
-cd "$DOT_FILES"
-git status -v 2>&1 | grep "nothing to commit" > /dev/null
+cd "/my-tools/dotfiles"
+git status 2>&1 | grep "nothing to commit" > /dev/null
 df_s="$?"
 
-hi="no"
-df="no"
-if [ "$hi_s" -eq "1" ];  then
-  hi="yes"
-fi
-
 if [ "$df_s" -eq "1" ];  then
-  df="yes"
+  echo -n "<span foreground='#ffffff' background='#ff0000'> DF </span>"
+else
+  echo -n "<span> DF </span>"
 fi
 
-echo "<span foreground='#ffffff' background='#ff0000'>DF: $df HI: $hi </span>"
+if [ "$hi_s" -eq "1" ];  then
+  echo -n "<span foreground='#ffffff' background='#ff0000'> HI </span>"
+else
+  echo -n "<span> HI </span>"
+fi
