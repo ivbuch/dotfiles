6509865867eeeddcbbe2676c3c1f37e42ce066e3
ivbuch <ivbuch@ivbuch>
2019-02-03 21:08:54 -0500

    update

--- i3/theme-changer/set-random-theme.sh
+++ i3/theme-changer/set-random-theme.sh
@@ -2,6 +2,8 @@
 home="/home/igor"
 wallpapers="$home/Pictures/wallpapers"
 wallpaper="$(find $wallpapers -type f -name "*.png" -or -name "*.jpg" | shuf | head -n 1)"
+wallpaper="/home/igor/workspace/dotfiles/i3/active-wallpaper.jpg"
+wallpaper="/home/igor/workspace/dotfiles/i3/active-wallpaper.png"
 log_file="/var/log/set-random-themes-logs/changes.log"
 
 $home/.config/virtual-envs/main/bin/wal -i "$wallpaper" >$log_file && i3-msg restart
