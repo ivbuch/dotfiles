91ab1c93bce8e2bbf0ac0230f45d516d0e885896
Igor Buchatskiy <igor.buchatskiy@medfusion.com>
2020-03-02 16:40:37 -0500

    bspwm update

--- polybar/scripts/pomodoro.sh
+++ polybar/scripts/pomodoro.sh
@@ -3,6 +3,7 @@
 pomodoro_client="/my-tools/home-infostructure/python-scripts/pomodoro/pomodoro-client.py"
 text="$(/home/igor/.config/virtual-envs/main/bin/python $pomodoro_client status 2>/tmp/aaa)"
 if [ -n "$text" ]; then
+  text="$(echo $text | sed 's/Pomodoro//')"
   echo "%{F#FF0000}🍅 $text%{F-}"
 fi
-echo "🍅 pomodoro disabled"
+echo "🍅 disabled"
