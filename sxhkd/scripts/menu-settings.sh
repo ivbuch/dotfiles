7036a7c47aae67d615cecaaec6333475c9a58ce6
Igor Buchatskiy <igor.buchatskiy@medfusion.com>
2020-03-18 11:37:06 -0400

    bspwm update

--- sxhkd/scripts/menu-settings.sh
+++ sxhkd/scripts/menu-settings.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-random_bg="set random background"
-show_help="show help"
-keymap_switch="enable keymap"
-restart_polybar="restart polybar"
-restart_wm="restart bspwm"
+keyboard_backlight() {
+  FN="/sys/class/leds/tpacpi::kbd_backlight/brightness"
+  CURRENT="$(cat $FN)"
+  MAX="$(cat /sys/class/leds/tpacpi::kbd_backlight/max_brightness)"
+  NEW="$((CURRENT + 1))"
+  if [ "$NEW" -gt "$MAX" ]; then
+    NEW="0"
+  fi
+  echo $NEW > $FN
+}
 
-txt=$(echo "$random_bg
-$show_help
-$keymap_switch
-$restart_polybar
-$restart_wm
+r_random_bg="set random background"
+r_show_help="show help"
+r_keymap_switch="enable keymap"
+r_restart_polybar="restart polybar"
+r_restart_wm="restart bspwm"
+r_keyboard_backlight="Keyboard backlight"
+
+txt=$(echo "$r_random_bg
+$r_show_help
+$r_keymap_switch
+$r_keyboard_backlight
+$r_restart_polybar
+$r_restart_wm
 " | rofi -dmenu ])
 
 if [ "$?" -ne 0 ] ; then
@@ -20,10 +33,10 @@ fi
 
 case "$txt" in
 
-  "$random_bg") systemctl start set-random-theme --user ;;
-  "$show_help") text2pdf ~/.config/sxhkd/sxhkdrc | zathura - ;;
-  "$keymap_switch") setxkbmap "us,ru" ",winkeys" "grp:caps_toggle" ;;
-  "$restart_polybar") /my-tools/dotfiles/polybar/bspwm-launch-polybar.sh ;;
-  "$restart_wm") bspc wm -r ;;
-
+  "$r_random_bg") systemctl start set-random-theme --user ;;
+  "$r_show_help") text2pdf ~/.config/sxhkd/sxhkdrc | zathura - ;;
+  "$r_keymap_switch") setxkbmap "us,ru" ",winkeys" "grp:caps_toggle" ;;
+  "$r_restart_polybar") /my-tools/dotfiles/polybar/bspwm-launch-polybar.sh ;;
+  "$r_restart_wm") bspc wm -r ;;
+  "$r_keyboard_backlight") keyboard_backlight ;;
 esac
