9bc051356d55bb6a9082d8349b8f1b18a741a983
Igor Buchatskiy <igor.buchatskiy@medfusion.com>
2020-03-20 14:54:13 -0400

    update

--- sxhkd/scripts/menu-settings.sh
+++ sxhkd/scripts/menu-settings.sh
@@ -17,26 +17,27 @@ r_keymap_switch="enable keymap"
 r_restart_polybar="restart polybar"
 r_restart_wm="restart bspwm"
 r_keyboard_backlight="Keyboard backlight"
+r_dns="Change DNS"
 
-txt=$(echo "$r_random_bg
+if ! txt=$(echo "$r_random_bg
 $r_show_help
 $r_keymap_switch
 $r_keyboard_backlight
 $r_restart_polybar
 $r_restart_wm
-" | rofi -dmenu ])
+$r_dns
+" | rofi -dmenu -i ) ; then
 
-if [ "$?" -ne 0 ] ; then
   echo "Bad choice"
   exit 0
 fi
 
 case "$txt" in
-
   "$r_random_bg") systemctl start set-random-theme --user ;;
   "$r_show_help") text2pdf ~/.config/sxhkd/sxhkdrc | zathura - ;;
   "$r_keymap_switch") setxkbmap "us,ru" ",winkeys" "grp:caps_toggle" ;;
   "$r_restart_polybar") /my-tools/dotfiles/polybar/bspwm-launch-polybar.sh ;;
   "$r_restart_wm") bspc wm -r ;;
   "$r_keyboard_backlight") keyboard_backlight ;;
+  "$r_dns") /my-tools/home-infostructure/dotfiles/bash/menu-dns-change.sh ;;
 esac
