e516f56db5ec34b7cff606e9ad01e4d14353abe8
Igor Buchatskiy <ivbuchatsky@gmail.com>
2020-04-03 00:22:00 -0400

    wireshark switch

--- polybar/scripts/wireguard-switch.sh
+++ polybar/scripts/wireguard-switch.sh
@@ -4,5 +4,6 @@ if ls /sys/class/net/wg0 1>/dev/null 2>/dev/null ; then
   notify-send "wireguard disabled"
 else
   sudo wg-quick up wg0
+  echo nameserver 192.168.2.1 | sudo tee /etc/resolv.conf > /dev/null
   notify-send "wireguard enabled"
 fi
