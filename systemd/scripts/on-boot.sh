79c91980f0512083bffbc127519da9906fcc31a7
Igor Buchatskiy <ivbuchatsky@gmail.com>
2020-04-04 22:37:56 -0400

    clipboard cache reset on hibernate/reboot

--- systemd/scripts/on-boot.sh
+++ systemd/scripts/on-boot.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+set -e
 
 echo "On poweroff script $(date)" >> /tmp/on-boot.log
 
-greenclip clear
+if [[ $DISPLAY ]] ; then 
+  pkill greenclip && greenclip clear 
+  echo "Greenclip cleared with display" >> /tmp/on-boot.log
+else
+  rm -f "$HOME/.cache/greenclip.history"
+  echo "Greenclip cleared with NO display" >> /tmp/on-boot.log
+fi
 
 echo "On poweroff script end $(date)" >> /tmp/on-boot.log
