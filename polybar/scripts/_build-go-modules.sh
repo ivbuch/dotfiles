11f99b263f635a8aaa90b2087cc297816c360ce7
Igor Buchatskiy <ivbuchatsky@gmail.com>
2020-04-26 22:35:23 -0400

    golang polybar modules

--- polybar/scripts/_build-go-modules.sh
+++ polybar/scripts/_build-go-modules.sh
@@ -3,7 +3,9 @@
 build_module() {
   output="$HOME/.bin/go-scripts"
   module_name="$1"
-  go build -o "$output/gi-$module_name" "$module_name/$module_name.go"
+  cd $1
+  go build -o "$output/gi-$module_name" "$module_name.go"
+  cd ..
 }
 
 build_module "module-borg"
@@ -11,3 +13,4 @@ build_module "module-docker"
 build_module "module-pacman"
 build_module "module-git"
 build_module "module-ip"
+build_module "module-internet"
---
+++ polybar/scripts/module-internet/module-internet.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"time"
+
+	"github.com/sparrc/go-ping"
+)
+
+func main() {
+	isInternetUp := isInternetUp()
+	isDNSOK := isDNSOK()
+	isPiholeOk := isPiholeOk()
+	isWireguardEnabled := isWireguardEnabled()
+
+	fmt.Printf("🌎  %s%s  📛%s 🔒%s ",
+		getIcon(isInternetUp),
+		getIcon(isDNSOK),
+		getIcon(isPiholeOk),
+		getIcon(isWireguardEnabled))
+}
+
+func isInternetUp() bool {
+	pinger, err := ping.NewPinger("8.8.8.8")
+	if err != nil {
+		log.Fatal(err)
+	}
+	pinger.Count = 1
+	pinger.Timeout = time.Second
+	pinger.Run()
+	stats := pinger.Statistics()
+	return stats.PacketsRecv == 1
+}
+
+func isDNSOK() bool {
+	_, err := net.LookupHost("google.com")
+	return err == nil
+}
+
+func isPiholeOk() bool {
+	addresses, err := net.LookupHost("googleadservices.com")
+	if err != nil {
+		return false
+	}
+
+	for _, v := range addresses {
+		if v == "0.0.0.0" {
+			return true
+		}
+	}
+	return false
+}
+
+func isWireguardEnabled() bool {
+	f := "/sys/class/net/wg0"
+	_, err := os.Stat(f)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+func getIcon(isGreen bool) string {
+	if isGreen {
+		return "🟢"
+	}
+	return "🔴"
+}
+
+func getIconWireguard(isGreen bool) string {
+	if isGreen {
+		return getIcon(isGreen)
+	}
+	return "🟡"
+}
