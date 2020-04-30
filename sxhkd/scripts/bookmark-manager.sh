4747f87568069d19883b7afd6dcbe36241f82f2a
Igor Buchatskiy <ivbuchatsky@gmail.com>
2020-04-30 10:07:36 -0400

    update

--- sxhkd/scripts/bookmark-manager.sh
+++ sxhkd/scripts/bookmark-manager.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-if selected=$(cat $HOME/.cache/bookmark-manager/work.txt  | rofi -dmenu -markup-rows -matching normal); then
+if selected=$(cat $HOME/.cache/bookmark-manager/work.txt  | rofi -i -dmenu --markup -markup-rows -matching normal -width 80); then
   link=$(echo "$selected" | awk -F '|' '{print $3}')
-  $1 "$link"
+
+  # restore & symbol in the url (not supported by rofi)
+  link=$(echo "$link" | sed 's|_VVV_|\&|g')
+  firefox "$link"
 fi
