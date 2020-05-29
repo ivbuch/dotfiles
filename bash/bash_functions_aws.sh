7bdcbac5eecc554216aa0ec8ce11c8e4218b6e17
Igor Buchatskiy <ivbuchatsky@gmail.com>
2020-05-28 21:20:54 -0400

    update

--- bash/bash_functions_aws.sh
+++ bash/bash_functions_aws.sh
@@ -1,13 +1,13 @@
 .aws_ec2_igor() {
   local query
-  query='.Reservations[].Instances[] | select (.KeyName != null) | select(.KeyName | contains("igor")) | {Name: .KeyName, NewName: (.Tags[] | select (.Key == "Name") | .Value), PublicIP: .PublicIpAddress}'
+  query='.Reservations[].Instances[] | select (.KeyName != null) | select(.KeyName | contains("igor")) | {Name: .KeyName, NewName: (.Tags[] | select (.Key == "Name") | .Value), PublicIP: .PublicIpAddress, State: .State.Name}'
 
   aws ec2 describe-instances  | jq "$query"
 }
 
 .aws_ec2_all() {
   local query
-  query='.Reservations[].Instances[] | {Name: .KeyName, NewName: (.Tags[] | select (.Key == "Name") | .Value), PublicIP: .PublicIpAddress}'
+  query='.Reservations[].Instances[] | {Name: .KeyName, NewName: (.Tags[] | select (.Key == "Name") | .Value), PublicIP: .PublicIpAddress, State: .State.Name}'
 
   aws ec2 describe-instances  | jq "$query"
 }
