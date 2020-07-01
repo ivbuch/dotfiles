70bebf6b485d39bc1dc4dabcd97a7c99985117d3
Igor Buchatskiy <ivbuchatsky@gmail.com>
2020-07-01 17:46:54 -0400

    update

--- bash/bash_aliases_k8s.sh
+++ bash/bash_aliases_k8s.sh
@@ -1,8 +1,5 @@
 alias k="kubectl"
 alias m="minikube"
 
-alias k_n="kubectl get nodes -o wide"
-alias k_a="kubectl get all -o wide"
-
-alias .k_igor_pod_create="kubectl run igor-network-utils --image=amouat/network-utils  ping 1.1.1.1"
-alias .k_igor_pod="kubectl exec -it igor-network-utils -- bash"
+alias .k8_igor_pod_create="kubectl run igor-network-utils --image=amouat/network-utils  ping 1.1.1.1"
+alias .k8_igor_pod="kubectl exec -it igor-network-utils -- bash"
