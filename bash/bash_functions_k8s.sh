ea43ff069f300569d993729e11517dc2fb72b2dc
Igor Buchatskiy <ivbuchatsky@gmail.com>
2020-08-07 18:30:31 -0400

    update

--- bash/bash_functions_k8s.sh
+++ bash/bash_functions_k8s.sh
@@ -1,3 +1,8 @@
-.k8_get_node_external_ip() {
-  kubectl get nodes -l kubernetes.io/role=node --no-headers -o json | jq '.items[] | select(.metadata.labels | contains({"kubernetes.io/role": "node"})) | .status.addresses[] | select(.type == "ExternalIP") | .address' --raw-output
+.k8_open_system_test() {
+  address=$(kubectl get nodes -l kubernetes.io/role=node --no-headers -o json | jq '.items[] | select(.metadata.labels | contains({"kubernetes.io/role": "node"})) | .status.addresses[] | select(.type == "ExternalIP") | .address' --raw-output)
+  firefox $address:30011/api/mds-static/tree.html
+}
+
+.k8_kafkacat() {
+   kubectl run kafkacat --image edenhill/kafkacat:1.6.0 --restart=Never --command -- sleep 1000000
 }
