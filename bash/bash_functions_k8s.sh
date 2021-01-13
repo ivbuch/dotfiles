26a8271b3e42230f87822ce0a3787fbaabd3d02f
Igor Buchatskiy <ivbuchatsky@gmail.com>
2021-01-13 08:44:32 -0500

    update

--- bash/bash_functions_k8s.sh
+++ bash/bash_functions_k8s.sh
@@ -13,3 +13,12 @@
   echo Consuming topic $topic
   kubectl exec -it kafkacat -- kafkacat -C -t $topic -o $offset -f '%t\n%p\n%o\n%R%s\n\n\n\n\n' -b z-kafka:9092 
 }
+
+.k8_pod_by_label() {
+  if [ -z "$1" ]; then
+    echo "Provide label selector like 'app=test'"
+    return 1
+  fi
+
+  kubectl get pods --selector "$1" --output json --field-selector=status.phase=Running | jq ".items[0].metadata.name" --raw-output
+}
