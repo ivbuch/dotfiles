101dc6c28e0354afd581434aa2d4874947ee5d3d
Igor Buchatskiy <ivbuchatsky@gmail.com>
2021-03-27 21:46:50 -0400

    pretty print rofi kill

--- sxhkd/scripts/rofi-kill.sh
+++ sxhkd/scripts/rofi-kill.sh
@@ -1,7 +1,16 @@
 #/bin/bash
 set -exuo pipefail
 
-if selected=$(ps -ef | sort | rofi -multi-select -dmenu -width 90 -fullscreen -matching normal); then
+programs=$(ps -ef)
+programs=$(echo "${programs}" | awk '
+{
+  username = $1
+  cmd = $8 $9 $10 $11 $12 $13 $14 $15
+  printf("%-12s | %s\n", username, cmd)
+}
+')
+
+if selected=$(echo "${programs}" | sort | rofi -multi-select -dmenu -width 90 -fullscreen -matching normal); then
   pids=$(echo "$selected" | awk '{print $2}')
   echo "Killing $selected"
   echo "Killing pids:"
