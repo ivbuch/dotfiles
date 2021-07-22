c8265039636568145af64c7f8228a91a6a4696a5
Igor Buchatskiy <ivbuchatsky@gmail.com>
2021-07-22 09:28:57 -0400

    helm complete

--- bash/zshrc.sh
+++ bash/zshrc.sh
@@ -23,6 +23,9 @@ cat ~/.cache/wal/sequences
 eval "$(direnv hook zsh)"
 
 source <(kubectl completion zsh)
+
+which helm 1>/dev/null 2>/dev/null && source <(helm completion zsh)
+
 complete -F __start_kubectl k
 complete -C '/usr/local/bin/aws_completer' aws
 complete -C cmon-completer cmon
