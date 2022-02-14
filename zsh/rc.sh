6f472275bbc63d9d02421a8d85de11e297e1f0a7
Igor Buchatskiy <ivbuchatsky@gmail.com>
2022-02-14 17:19:03 -0500

    zsh

--- zsh/rc.sh
+++ zsh/rc.sh
@@ -87,9 +87,15 @@ autoload bashcompinit
 bashcompinit
 
 bindkey -v
-bindkey '\e[A' history-search-backward
-bindkey '\e[B' history-search-forward
-# bindkey '^R' history-incremental-search-backward
+
+# search with arrors up/down
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
+
 bindkey '^R' history-incremental-pattern-search-backward
 
 source /my-tools/dotfiles/zsh/rc-override.sh
