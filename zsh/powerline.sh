800efce3f4e1844220389a67ad64b79dd4f618b3
Igor Buchatskiy <ivbuchatsky@gmail.com>
2022-02-14 17:45:55 -0500

    zsh

--- zsh/powerline.sh
+++ zsh/powerline.sh
@@ -48,38 +48,6 @@ if [ "$POWERLINE_CUSTOM_CURRENT_PATH" != "" ]; then
   POWERLINE_CURRENT_PATH="$POWERLINE_CUSTOM_CURRENT_PATH"
 fi
 
-if [ "$POWERLINE_GIT_CLEAN" = "" ]; then
-  POWERLINE_GIT_CLEAN="✔"
-fi
-
-if [ "$POWERLINE_GIT_DIRTY" = "" ]; then
-  POWERLINE_GIT_DIRTY="✘"
-fi
-
-if [ "$POWERLINE_GIT_ADDED" = "" ]; then
-  POWERLINE_GIT_ADDED="%F{green}✚%F{black}"
-fi
-
-if [ "$POWERLINE_GIT_MODIFIED" = "" ]; then
-  POWERLINE_GIT_MODIFIED="%F{blue}✹%F{black}"
-fi
-
-if [ "$POWERLINE_GIT_DELETED" = "" ]; then
-  POWERLINE_GIT_DELETED="%F{red}✖%F{black}"
-fi
-
-if [ "$POWERLINE_GIT_UNTRACKED" = "" ]; then
-  POWERLINE_GIT_UNTRACKED="%F{yellow}✭%F{black}"
-fi
-
-if [ "$POWERLINE_GIT_RENAMED" = "" ]; then
-  POWERLINE_GIT_RENAMED="➜"
-fi
-
-if [ "$POWERLINE_GIT_UNMERGED" = "" ]; then
-  POWERLINE_GIT_UNMERGED="═"
-fi
-
 if [ "$POWERLINE_RIGHT_A_COLOR_FRONT" = "" ]; then
   POWERLINE_RIGHT_A_COLOR_FRONT="white"
 fi
@@ -87,42 +55,6 @@ fi
 if [ "$POWERLINE_RIGHT_A_COLOR_BACK" = "" ]; then
   POWERLINE_RIGHT_A_COLOR_BACK="black"
 fi
-
-ZSH_THEME_GIT_PROMPT_PREFIX=" \ue0a0 "
-ZSH_THEME_GIT_PROMPT_SUFFIX=""
-ZSH_THEME_GIT_PROMPT_DIRTY=" $POWERLINE_GIT_DIRTY"
-ZSH_THEME_GIT_PROMPT_CLEAN=" $POWERLINE_GIT_CLEAN"
-
-ZSH_THEME_GIT_PROMPT_ADDED=" $POWERLINE_GIT_ADDED"
-ZSH_THEME_GIT_PROMPT_MODIFIED=" $POWERLINE_GIT_MODIFIED"
-ZSH_THEME_GIT_PROMPT_DELETED=" $POWERLINE_GIT_DELETED"
-ZSH_THEME_GIT_PROMPT_UNTRACKED=" $POWERLINE_GIT_UNTRACKED"
-ZSH_THEME_GIT_PROMPT_RENAMED=" $POWERLINE_GIT_RENAMED"
-ZSH_THEME_GIT_PROMPT_UNMERGED=" $POWERLINE_GIT_UNMERGED"
-ZSH_THEME_GIT_PROMPT_AHEAD=" ⬆"
-ZSH_THEME_GIT_PROMPT_BEHIND=" ⬇"
-ZSH_THEME_GIT_PROMPT_DIVERGED=" ⬍"
-
-# if [ "$(git_prompt_info)" = "" ]; then
-   # POWERLINE_GIT_INFO_LEFT=""
-   # POWERLINE_GIT_INFO_RIGHT=""
-# else
-    if [ "$POWERLINE_SHOW_GIT_ON_RIGHT" = "" ]; then
-        if [ "$POWERLINE_HIDE_GIT_PROMPT_STATUS" = "" ]; then
-            POWERLINE_GIT_INFO_LEFT="%K{white}%F{white}%F{black}%K{white}"$'$(git_prompt_info)$(git_prompt_status)%F{white}'
-        else
-            POWERLINE_GIT_INFO_LEFT=" %F{blue}%K{white}"$'\ue0b0'"%F{white}%F{black}%K{white}"$'$(git_prompt_info)%F{white}'
-        fi
-        POWERLINE_GIT_INFO_RIGHT=""
-    else
-        POWERLINE_GIT_INFO_LEFT=""
-        if [ "$POWERLINE_HIDE_GIT_PROMPT_STATUS" = "" ]; then
-            POWERLINE_GIT_INFO_RIGHT="%F{white}"$'\ue0b2'"%F{black}%K{white}"$'$(git_prompt_info)$(git_prompt_status)'" %K{white}"
-        else
-            POWERLINE_GIT_INFO_RIGHT="%F{white}"$'\ue0b2'"%F{black}%K{white}"$'$(git_prompt_info)'" %K{white}"
-        fi
-    fi
-# fi
 
 if [ $(id -u) -eq 0 ]; then
     POWERLINE_SEC1_BG=%K{red}
@@ -147,17 +79,7 @@ else
     VENV_STATUS=""
 fi
 
-PROMPT=""$POWERLINE_GIT_INFO_LEFT" %k"" "
-
-if [ "$POWERLINE_DISABLE_RPROMPT" = "" ]; then
-    if [ "$POWERLINE_RIGHT_A" = "" ]; then
-        RPROMPT="$POWERLINE_GIT_INFO_RIGHT%F{white}"$'\ue0b2'"%k%F{black}%K{white} $POWERLINE_RIGHT_B %f%k"
-    elif [ "$POWERLINE_RIGHT_B" = "" ]; then
-        RPROMPT="$POWERLINE_GIT_INFO_RIGHT%F{white}"$'\ue0b2'"%k%F{$POWERLINE_RIGHT_A_COLOR_FRONT}%K{$POWERLINE_RIGHT_A_COLOR_BACK} $POWERLINE_RIGHT_A %f%k"
-    else
-        RPROMPT="$POWERLINE_GIT_INFO_RIGHT%F{white}"$'\ue0b2'"%k%F{black}%K{white} $POWERLINE_RIGHT_B %f%F{$POWERLINE_RIGHT_A_COLOR_BACK}"$'\ue0b2'"%f%k%K{$POWERLINE_RIGHT_A_COLOR_BACK}%F{$POWERLINE_RIGHT_A_COLOR_FRONT} $POWERLINE_RIGHT_A %f%k"
-    fi
-fi
+PROMPT="%k"" "
 
 precmd() {
   exit_status="$?"
--- zsh/rc-override.sh
+++ zsh/rc-override.sh
@@ -1,7 +1,6 @@
 source "${HOME}/.bash_aliases"
 source "${DOT_FILES}/bash/bash_functions.sh"
 
-
 VIRTUAL_ENV_DISABLE_PROMPT=1 source $HOME/.config/virtual-envs/main/bin/activate
 source "${HOME_INFRA}/bash-scripts/z.sh"
 
