dff2474f71414fb204bde91cc856be49d75b7307
Igor Buchatskiy <ivbuchatsky@gmail.com>
2022-04-25 12:13:07 -0400

    disable pywal and set new vim color scheme

--- systemd/scripts/set-random-theme/set-random-theme.sh
+++ systemd/scripts/set-random-theme/set-random-theme.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 set -e
 
-wallpapers="$HOME/pictures/wallpapers"
-wallpaper="$(find $wallpapers -type f -name "*.png" -or -name "*.jpg" | shuf | head -n 1)"
-log_file="/var/log/set-random-themes-logs/changes.log"
-
-echo $wallpaper
-
-$HOME/.config/virtual-envs/main/bin/wal -i "$wallpaper" >$log_file
-echo " $(date) $wallpaper" >> "$log_file"
-
-xrdb ~/.cache/wal/colors.Xresources
+wallpapers="${HOME}/pictures/wallpapers"
+wallpaper="$(find ${wallpapers} -type f -name "*.png" -or -name "*.jpg" | shuf | head -n 1)"
+echo "${wallpaper}"
+feh --bg-fill "${wallpaper}"
