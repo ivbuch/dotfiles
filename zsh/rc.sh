a8b0d68556af1a0ffc7bf53ddea28b7b7e754af8
Igor Buchatskiy <ivbuchatsky@gmail.com>
2022-12-22 16:45:38 -0500

    zsh completition

--- zsh/rc.sh
+++ zsh/rc.sh
@@ -67,12 +67,13 @@ complete -F __start_kubectl k
 complete -C '/usr/local/bin/aws_completer' aws
 complete -C cmon-completer cmon
 
-if [ "$(hostname)" = "igordesk" ]; then
+if [ "$(hostname)" = "igor-legion" ]; then
   source /usr/local/ibmcloud/autocomplete/zsh_autocomplete
   # ~/.bin/go-scripts/jenkins-cli completion zsh > "${fpath[1]}/_jenkins-cli"
   # i-kafka-consumer completion zsh > "${fpath[1]}/_i-kafka-consumer"
   # i-protobuf-decoder completion zsh > "${fpath[1]}/_i-protobuf-decoder"
   # i-postgres-cli completion zsh > "${fpath[1]}/_i-postgres-cli"
+  sdc-rest completion zsh > "${fpath[1]}/_sdc-rest"
 fi
 
 # PageDuty autocomplete
